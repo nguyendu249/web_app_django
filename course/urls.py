45b9d0143c6886936d039cd68a6056e56d6372fb
nguyensang02610 <nguyensang02610@gmail.com>
2022-10-03 08:29:21 +0700

    view course

--- course/urls.py
+++ course/urls.py
@@ -6,5 +6,6 @@ app_name = 'course'
 urlpatterns = [
     path('', views.CourseList, name='courselist'),
     path('<slug:slug>/',views.CourseDetails, name='course_details'),
-    path('learn/<slug:slug>/',views.LearnCourse, name='learn')
+    path('learn/<slug:slug>/',views.LearnCourse, name='learn'),
+    path('<int:course_id>/lecture/<int:id>/',views.LeesonView, name='learn_view')
 ]
--- course/views.py
+++ course/views.py
@@ -18,6 +18,7 @@ def LearnCourse(request, slug):
     for topic in topics:
         for item in Lesson.objects.filter(topic_id = topic.id).order_by('id'):
             lesson_aray.append(item)
+    first_lesson = lesson_aray[0].video
     # lessons = Lesson.objects.all().order_by('id')
     # for topic in topics:
     #     for lesson in lessons:
@@ -28,4 +29,26 @@ def LearnCourse(request, slug):
         {'course' : course,
         'topics' : topics,
         'lessons' : lesson_aray,
+        'first_lesson' : first_lesson
+
+    })
+
+def LeesonView(request,course_id ,id ):
+    course = Course.objects.get(id = course_id)
+    topics = Topic.objects.filter(course_id = course.id).order_by('id')
+    #lấy tất cả các lesson
+    lesson_aray = []
+    for topic in topics:
+        for item in Lesson.objects.filter(topic_id = topic.id).order_by('id'):
+            lesson_aray.append(item)
+            if item.id == id:
+                lesson_select = item
+    
+    first_lesson = lesson_select
+    return render(request,'course_learn.html',
+        {'course' : course,
+        'topics' : topics,
+        'lessons' : lesson_aray,
+        'first_lesson' : first_lesson
+
     })
