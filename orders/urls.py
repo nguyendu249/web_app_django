fbe882f48efe0908158d704d5796788db03b3564
nguyensang02610 <nguyensang02610@gmail.com>
2023-01-07 00:09:35 +0700

    vnpay

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from orders import views
 
 app_name = 'orders'
 urlpatterns = [
+    url(r'^payment$', views.payment, name='payment'),
+    path('', views.CourseList.as_view(), name='courselist'),
     path('place', views.CreateOrder.as_view(), name='place'),
     path('my-course', views.MyOrders.as_view(), name='my_course'),
     path('details/<int:pk>/', views.OrderDetails.as_view(), name='details'),
